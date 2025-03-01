c6bd5aabd73ca5fb49ad3129a7dab24f17b45cbe
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-03-01 19:09:09 -0300

    add arquivos python

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,11 +1,15 @@
 from loguru import logger
 
-logger.add("meu_app.log")
+logger.add("meu_app.log", level="CRITICAL")
 
 def soma(x, y):
-    logger.info(x)
-    logger.info(y)
-    logger.info(x + y)
-    return x + y
+    try:
+        soma = x + y
+        logger.info(f"Voce digitou valores corretor {soma}")
+        return soma
+    except:
+        logger.critical("Voce tem que digitar valores corretor")
 
-soma(2,3)
+soma(2,"3")
+
+soma(2, 8)
